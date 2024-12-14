6a3c93cebff4c513331881582b3579a1f54c74a1
anhpi1 <phamhaianh660@gmail.com>
2024-12-14 14:00:38 +0700

    haianh

---
+++ Benfinit_water/View/ctrl_danh_sach_co_so.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Benfinit_water.View
+{
+    /// <summary>
+    /// Interaction logic for ctrl_danh_sach_co_so.xaml
+    /// </summary>
+    public partial class ctrl_danh_sach_co_so : UserControl
+    {
+        public ctrl_danh_sach_co_so()
+        {
+            InitializeComponent();
+        }
+    }
+}
